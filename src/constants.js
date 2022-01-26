4ef12334d4f814ab565bddc7928a940efa1b0e36
eigen <jingyu.pang@tufts.edu>
2022-01-26 18:51:51 -0500

    added world map

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SAT_API_KEY = "F2CRBY-YYBPEH-7VDYH7-4RVP";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
